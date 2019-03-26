9dc417a9a78f1c93300ce7051c389c237fc28cf3
F44704C <F44704C@auth.fcagroup.com>
2019-03-26 14:47:24 +0100

    Add project files.

---
+++ AllTasks/Controllers/ProjectController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using AllTasks.Models;
+namespace AllTasks.Controllers
+
+{
+    public class ProjectController : Controller
+    {
+        private TasksDBContext db = new TasksDBContext();
+
+        // GET: Project
+        public async System.Threading.Tasks.Task<ActionResult> Index(string Project_Name,string searchString)
+        {
+            string pn = Project_Name;
+            var movies = from m in db.Tasks
+                         select m;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            else
+            {
+                movies = movies.Where(s => s.Project_Name.Contains(pn));
+
+            }
+            return View(await movies.ToListAsync());
+
+
+        }
+
+
+
+        // GET: Project/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // GET: Project/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Project/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nr,Title,Description,Worked,T_L,Test_Date,My_Time,Project_Name")] tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(tasks);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { tasks.Project_Name });
+            }
+
+            return View(tasks);
+        }
+
+        // GET: Project/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            tasks.Test_Date = DateTime.Now;
+            return View(tasks);
+        }
+
+        // POST: Project/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nr,Title,Description,Worked,T_L,Test_Date,My_Time,Project_Name")] tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tasks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index",new { tasks.Project_Name});
+            }
+            return View(tasks);
+        }
+
+        // GET: Project/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // POST: Project/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tasks tasks = db.Tasks.Find(id);
+            db.Tasks.Remove(tasks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AllTasks/Models/Tasks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace AllTasks.Models
+{
+    public class tasks
+    {
+        public int ID { get; set; }
+        [DisplayName("Task Nr")]
+        public int Nr { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Worked { get; set; }
+        [DisplayName("Tested L")]
+        public bool T_L { get; set; }
+        [DisplayName("Test Date")]
+        [DataType(DataType.Date)]
+        public DateTime Test_Date { get; set; }
+        [DisplayName("My Time")]
+        public decimal My_Time{ get; set; }
+        [DisplayName("Project Name")]
+        public string Project_Name{ get; set; }
+    }
+
+    public class TasksDBContext : DbContext
+    {
+        public DbSet<tasks> Tasks { get; set; }
+    }
+}
